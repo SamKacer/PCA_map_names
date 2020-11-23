93c13072d4e0190f951f4f0f3363699c2f9213cd
Samuel Kacer <2093358k@student.gla.ac.uk>
2020-11-23 14:12:03 +0100

    Add script for generating map names

---
+++ gen_map_names.py
@@ -0,0 +1,34 @@
+import re
+import sys
+
+def extract_map_name(line: str):
+    match = re.search("mapgroup (.+),.+,", line)
+    if not match: return None
+    else: return match.group(1)
+
+def format_name(raw_name: str) -> str:
+    return raw_name.replace('_', ' ').lower().replace(' rb', '').title()
+
+#assume is formatted
+def output_map_name(map_id, map_name):
+    print (f"  [{map_id}] = " + "{")
+    print(f"    map = \"{map_name}\"")
+    print("  },")
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    group_number = int(sys.argv[2])
+    map_number = 1
+    with open(filename) as f:
+        print('{')
+        for line in f:
+            if 'newgroup' in line:
+                group_number += 1
+                map_number = 1
+            else:
+                maybe_map_name = extract_map_name(line)
+                if not maybe_map_name: continue
+                map_id = group_number * 256 + map_number
+                map_number += 1
+                output_map_name(map_id, format_name(maybe_map_name))
+        print('}')
